adf26d702bc36b3326eb365589d2d2e76e897df4
hello <hello@hello.hello>
2023-07-31 17:44:29 +0800

    second commit

--- src/main/java/com/hello/controller/TestController.java
+++ src/main/java/com/hello/controller/TestController.java
@@ -3,5 +3,6 @@ package com.hello.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("git");
+        System.out.println("hello world!");
     }
 }
