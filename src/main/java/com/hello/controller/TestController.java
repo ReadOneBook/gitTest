7a08cd00916774b4bbeae9bb041d0b5a729fb197
ReadOneBook <103417130+ReadOneBook@users.noreply.github.com>
2023-08-01 19:36:34 +0800

    GitHub commit

--- src/main/java/com/hello/controller/TestController.java
+++ src/main/java/com/hello/controller/TestController.java
@@ -2,8 +2,6 @@ package com.hello.controller;
 
 public class TestController {
     public static void main(String[] args) {
-        System.out.println("git");
-        System.out.println("hello world!");
         System.out.println("hot-fix");
         System.out.println("hello world!");
     }
